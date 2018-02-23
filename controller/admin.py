1c0b2f4b835faf673693a2702807670f41a2f19b
Amir_Mohammad <amir_day1374@yahoo.com>
2018-02-23 12:23:58 +0330

    set limit for log

--- controller/admin.py
+++ controller/admin.py
@@ -11,7 +11,6 @@ from models.user import User
 from models.log import Log
 from controller import app
 from controller.extension import db
-from garbage import create_user_id
 
 __Author__ = "Amir Mohammad"
 
@@ -32,7 +31,7 @@ def add_admin():
         brand = request.form['brand']
         category = request.form['category']
 
-        user_obj = User(id=create_user_id(), username=username, passwordhash=passwordhash,
+        user_obj = User(username=username, passwordhash=passwordhash,
                         firstname=firstname, lastname=lastname, gender=gender, phones=phones,
                         brand=brand, category=category)
 
@@ -68,8 +67,32 @@ def show_log_name(username):
 def show_log_all():
     if User.logged_in_user() == "tecvest@1010":
         log_obj_q = Log.query.all()
+        row_count = Log.query.count()
+
+        if not 3 < row_count <= 12:
+            print "the member of log : ", row_count
+            limit_log_person = Log.query.limit(2).all()
+            print "the id is : ", limit_log_person[1].id
+            print "the name is : ", limit_log_person[1].user_id
+            print "the action is : ", limit_log_person[1].action
+            me = limit_log_person[1]
+            db.session.delete(me)
+            db.session.commit()
+        else:
+            row_count = Log.query.count()
+            print "the member of log : ", row_count
+            # if row_count<=100:
+            #     for inline in limit_log_person:
+            #         deleted = inline.action
+            #     print deleted
+            # User.query.filter_by(id=inline.id).delete()
+            # print jsonify(logs=[i.to_json(with_user=True) for i in log_obj_q]), 200
+
+            # User.query.filter_by(id=inline.id).delete()
+            # db.session.commit()
         # print jsonify(logs=[i.to_json(with_user=True) for i in log_obj_q]), 200
         # print "action is : ", log_obj_q.action
+
         # print "time is : ", log_obj_q.time
         return render_template('show_user_date.html', log_obj_q=log_obj_q)
     else:
