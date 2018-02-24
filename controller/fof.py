ae10a5f14c00e19ca4ec983b58b7ac40d4a56e21
Amir_Mohammad <amir_day1374@yahoo.com>
2018-02-24 15:24:46 +0330

    UI bugs fixed

--- controller/fof.py
+++ controller/fof.py
@@ -12,5 +12,5 @@ from controller import app
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html')
+def page_not_found(error):
+    return render_template('404.html'), 404
